d25d8f9c87be1d368e3fc712a91fcd16f78bcefd
Murad Alhassen <murad.alhassen1@gmail.com>
2026-02-14 12:53:22 -0700

    Revert base path to / for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/studio-east-spark/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
